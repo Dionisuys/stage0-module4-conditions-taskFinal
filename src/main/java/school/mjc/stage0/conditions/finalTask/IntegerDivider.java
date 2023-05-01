8a4afc92cbad4b4bec913566336dfe8088134a5d
Dzianis Panasiuk <dzianispanasiuk@gmail.com>
2023-05-02 00:48:09 +0300

    Module #4.1. Final Task solution

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        if (dividend > 0 && divider > 0) {
+            int result = 0;
+            result = dividend / divider;
+            System.out.println((result * divider == dividend) ?
+                    "can be divided completely" :
+                    "cannot be divided completely");
+        } else {
+            System.out.println("division by zero");
+        }
     }
 }
